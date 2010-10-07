a0c06c8c7a3862776cabf45e8c77f656de2027b9
Marc Egli <frog32@me.com>
2010-10-07 10:16:31 +0200

    initial commit

---
+++ newsletter/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Newsletter(models.Model):
+    pass
---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+from setuptools import setup
+setup(
+    name='newsletter',
+    version = newsletter.__version__,
+    description = 'django based newsletter toolkit',
+    author = 'Marc Egli',
+    author_email = 'egli@allink.ch',
+    url = 'http://github.com/frog32/'
+)
