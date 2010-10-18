1ad9a4c9e46f670a9c7581c5057cfee96efd877e
Marc Egli <frog32@me.com>
2010-10-18 17:35:43 +0200

    modified some settings

--- pennyblack/__init__.py
+++ pennyblack/__init__.py
@@ -1,1 +1,2 @@
-__version__ = '0.1'
+VERSION = (0, 0, 1)
+__version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
 #! /usr/bin/env python
 from setuptools import setup
+
+import pennyblack
 setup(
-    name='newsletter',
-    version = newsletter.__version__,
+    name='pennyblack',
+    version = pennyblack.__version__,
     description = 'django based newsletter toolkit',
     author = 'Marc Egli',
     author_email = 'egli@allink.ch',
-    url = 'http://github.com/frog32/'
+    url = 'http://github.com/allink/pennyblack/',
+    packages=['pennyblack', 'pennyblack.conf', 'pennyblack.content'],
+    package_data={'pennyblack':'templates/*.html'},
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Communications :: Email',
+    ],
+    requires=[
+        'FeinCMS(>=1.1.4)',
+        'Django(>=1.2.1)',
+    ],
+    
 )
