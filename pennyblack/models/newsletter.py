d54de69dc701d2ffcde58ec04bfb910c51128423
Marc Egli <frog32@me.com>
2011-02-21 08:45:13 +0100

    fixed contenttype import

--- pennyblack/models/newsletter.py
+++ pennyblack/models/newsletter.py
@@ -3,6 +3,7 @@ import exceptions
 
 from django.conf.urls.defaults import patterns, url
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models import signals
 from django.utils import translation
