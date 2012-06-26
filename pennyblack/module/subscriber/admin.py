27eed87ac2963b123ddc72cc71efaaef9ba0b026
Marc Egli <frog32@me.com>
2012-06-26 16:09:46 +0200

    add django-admin-import to subscriber module

--- pennyblack/module/subscriber/admin.py
+++ pennyblack/module/subscriber/admin.py
@@ -5,3 +5,13 @@ from pennyblack.module.subscriber.models import NewsletterSubscriber,\
 
 admin.site.register(NewsletterSubscriber, NewsletterSubscriberAdmin)
 admin.site.register(SubscriberGroup, SubscriberGroupAdmin)
+
+try:
+    from admin_import.options import add_import
+except ImportError, e:
+    if not e.message == 'No module named admin_import.options':
+        raise
+else:
+    from django.conf import settings as django_settings
+    if 'admin_import' in django_settings.INSTALLED_APPS:
+        add_import(NewsletterSubscriberAdmin, add_button=True)
